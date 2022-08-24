2503cb8843c357e7139f18ede69388b433e869f1
khushiv30 <khushi.varshney_cs19@gla.ac.in>
2022-08-24 15:14:39 +0530

    apicall file changes made

--- Day-15/4-apicall.js
+++ Day-15/4-apicall.js
@@ -2,62 +2,117 @@
 // 2. fetch()
 // 3. Third Party Package (Axios)
 
-// XHR
+// // XHR
 
-const API_URL="https://jsonplaceholder.typicode.com/users";
+// const API_URL="https://jsonplaceholder.typicode.com/users";
 
-function getApiData(url){
-    console.log("We are good to go to call API using XHR");
-    return new Promise((resolve,reject)=>{
+// function getApiData(url){
+//     console.log("We are good to go to call API using XHR");
+//     return new Promise((resolve,reject)=>{
 
-        //create an instance of XHR
-        var request =new XMLHttpRequest();
+//         //create an instance of XHR
+//         var request =new XMLHttpRequest();
 
-        request.open("GET",url);
-        request.send();
+//         request.open("GET",url);
+//         request.send();
 
-        request.onload=()=>{
-            // console.log(request.response);
-            // console.log(request);
+//         request.onload=()=>{
+//             // console.log(request.response);
+//             // console.log(request);
 
-            if(request.status===200){
-                resolve(request.response)
-            }
-            else{
-                reject("Not able to fetch data");
-            }
-        }
-    });
+//             if(request.status===200){
+//                 resolve(request.response)
+//             }
+//             else{
+//                 reject("Not able to fetch data");
+//             }
+//         }
+//     });
+// }
+
+// getApiData(API_URL)
+// .then((val)=>{
+//     console.log(JSON.parse(val));
+//     displayData(JSON.parse(val));
+// })
+// .catch((err)=>console.log("Error"+err));
+
+// function displayData(serverData){
+//     // console.log(serverData)
+//     for(var user of serverData){
+//         let newtr=document.createElement("tr");
+//         let newtd1=document.createElement('td');
+//         let newtd2=document.createElement('td');
+//         let newtd3=document.createElement('td');
+//         let newtd4=document.createElement('td');
+
+//         // newtd1.innerText=`$(user.id)`;
+//         newtd1.innerText=""+user.id;
+//         newtd1.innerText=""+user.name;
+//         newtd1.innerText=""+user.email;
+//         newtd1.innerText=""+user.phone;
+
+//         newtr.appendChild(newtd1);
+//         newtr.appendChild(newtd2);
+//         newtr.appendChild(newtd3);
+//         newtr.appendChild(newtd4);
+
+//         document.getElementById('myTable').appendChild(newtr);
+//     }
+// }
+
+
+
+// 1. XHR
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+function getApiData(url) {
+  console.log("We are good to go to call API using XHR ");
+  return new Promise((resolve, reject) => {
+    // create an instance of XHR
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.send();
+
+    request.onload = () => {
+      console.log(request);
+
+      if (request.status === 200) {
+        resolve(request.response);
+      } else {
+        reject("Not able to fetch Data");
+      }
+    };
+  });
 }
 
 getApiData(API_URL)
-.then((val)=>{
+  .then((val) => {
     console.log(JSON.parse(val));
     displayData(JSON.parse(val));
-})
-.catch((err)=>console.log("Error"+err));
+  })
+  .catch((err) => console.log("Error" + err));
 
-function displayData(serverData){
-    // console.log(serverData)
-    for(var user of serverData){
-        let newtr=document.createElement("tr");
-        let newtd1=document.createElement('td');
-        let newtd2=document.createElement('td');
-        let newtd3=document.createElement('td');
-        let newtd4=document.createElement('td');
+function displayData(serverData) {
+  for (var user of serverData) {
+    let newtr = document.createElement("tr");
 
-        // newtd1.innerText=`$(user.id)`;
-        newtd1.innerText=""+user.id;
-        newtd1.innerText=""+user.name;
-        newtd1.innerText=""+user.email;
-        newtd1.innerText=""+user.phone;
+    let newtd1 = document.createElement("td");
+    let newtd2 = document.createElement("td");
+    let newtd3 = document.createElement("td");
+    let newtd4 = document.createElement("td");
 
-        newtr.appendChild(newtd1);
-        newtr.appendChild(newtd2);
-        newtr.appendChild(newtd3);
-        newtr.appendChild(newtd4);
+    newtd1.innerText = "" + user.id;
+    newtd2.innerText = "" + user.name;
+    newtd3.innerText = "" + user.email;
+    newtd4.innerText = "" + user.phone;
 
-        document.getElementById('myTable').appendChild(newtr);
-    }
+    newtr.appendChild(newtd1);
+    newtr.appendChild(newtd2);
+    newtr.appendChild(newtd3);
+    newtr.appendChild(newtd4);
+
+    document.getElementById("myTable").appendChild(newtr);
+  }
 }
 
